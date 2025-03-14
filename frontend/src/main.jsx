23601374965b4e0085d1a3974f55b2cb27108799
Stephanie <sgage2013@gmail.com>
2025-03-13 19:58:47 -0500

    finished phase 0

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,9 +2,27 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import React from 'react'
+import ReactDom from 'react-dom'
+import { Provider } from 'react-redux'
+import configureStore from './store/store.js'
+import { restoreCSRF, csrfFetch } from './store/csrf.js'
 
+const store = configureStore();
+if (import.meta.env.MODE !== 'production'){
+  restoreCSRF();
+  window.csrfFetch = csrfFetch;
+  window.store = store;
+}
+if (process.env.NODE_ENV !== 'production'){
+  window.store=store;
+}
 createRoot(document.getElementById('root')).render(
   <StrictMode>
+    <Provider store={store}>
     <App />
+  </Provider>
   </StrictMode>,
 )
+
+
