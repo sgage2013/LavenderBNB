0d8e879c0ec1195e196f5e7d2340d841e9f68ad7
Stephanie <sgage2013@gmail.com>
2025-03-14 21:00:46 -0500

    finished, need to deploy

--- backend/db/migrations/01-user.js
+++ backend/db/migrations/01-user.js
@@ -49,6 +49,6 @@ module.exports = {
   },
   async down(queryInterface, Sequelize) {
     options.tableName = "Users";
-    return queryInterface.dropTable(options);
+    return queryInterface.dropTable('Users');
   }
 };
